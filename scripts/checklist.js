ac189d9ce018373e5359f564f89a129ed922a688
andreykoleber <andrei.koleber@gmail.com>
2018-10-24 23:14:18 +0600

    Add remove and add row functionality

---
+++ scripts/checklist.js
@@ -0,0 +1,64 @@
+(function (window) {
+  'use strict';
+
+  var App = window.App || {},
+    $ = window.jQuery;
+
+  function Row(coffeeOrder) {
+    var $div = $('<div></div>', {
+        'data-coffee-order': 'checkbox',
+        'class': 'checkbox'
+      }),
+
+      $label = $('<label></label>'),
+
+      $checkbox = $('<input></input>', {
+        type: 'checkbox',
+        value: coffeeOrder.emailAddress
+      }),
+
+      description = coffeeOrder.size + ' ';
+
+    if (coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + ' ';
+    }
+
+    description += coffeeOrder.coffee + ', ';
+    description += ' (' + coffeeOrder.emailAddress + ')';
+    description += ' [' + coffeeOrder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+    this.$element = $div;
+  }
+
+  function CheckList(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+
+    this.$element = $(selector);
+    if (this.$element.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+  }
+
+  CheckList.prototype.addRow = function (coffeeOrder) {
+    //Remove any existing rows that match the email address
+    this.removeRow(coffeeOrder.emailAddress);
+    var rowElement = new Row(coffeeOrder);
+    //Add the new row instamce's $element property to the checklist
+    this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function (email) {
+    console.log("hell");
+    this.$element
+      .find('[value="' + email + '"]')
+      .closest('[data-coffee-order="checkbox"]').remove();
+  };
+
+  App.CheckList = CheckList;
+  window.App = App;
+}(window));
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
+/*global console */
+
 (function (window) {
   'use strict';
-  var App = window.App,
+  var FORM_SELECTOR = '[data-coffee-order="form"]',
+    CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]',
+
+    App = window.App,
     Truck = App.Truck,
     DataStore = App.DataStore,
-    myTruck = new Truck('ncc-1701', new DataStore());
+    FormHandler = App.FormHandler,
+    CheckList = App.CheckList,
+    myTruck = new Truck('ncc-1701', new DataStore()),
+    checkList = new CheckList(CHECKLIST_SELECTOR),
+    formHandler = new FormHandler(FORM_SELECTOR);
+
+  window.myTruck = myTruck;
+  formHandler.addSubmitHandler(function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
 
   window.myTruck = myTruck;
 }(window));
