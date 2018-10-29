fa1beccc8d987e287fd9b75c6ae45b6d476b5151
andreykoleber <andrei.koleber@gmail.com>
2018-10-29 22:12:51 +0600

    Add form validation

--- scripts/main.js
+++ scripts/main.js
@@ -10,16 +10,27 @@
     DataStore = App.DataStore,
     FormHandler = App.FormHandler,
     CheckList = App.CheckList,
+    Validation = App.Validation,
     myTruck = new Truck('ncc-1701', new DataStore()),
     checkList = new CheckList(CHECKLIST_SELECTOR),
-    formHandler = new FormHandler(FORM_SELECTOR);
+    formHandler = new FormHandler(FORM_SELECTOR),
+    webshim = window.webshim;
+
+  window.myTruck = myTruck;
 
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
-  window.myTruck = myTruck;
+
   formHandler.addSubmitHandler(function (data) {
     myTruck.createOrder.call(myTruck, data);
     checkList.addRow.call(checkList, data);
   });
 
+  formHandler.addInputHandler(Validation.isCompanyEmail);
   window.myTruck = myTruck;
+  webshim.polyfill('forms forms-ext');
+  webshim.setOptions('forms', {
+    addValidators: true,
+    lazyCustomMessages: true
+  });
+
 }(window));
---
+++ scripts/validation.js
@@ -0,0 +1,12 @@
+(function (window) {
+  'use strict';
+  var App = window.App || {},
+    Validation = {
+      isCompanyEmail: function (email) {
+        return (/.+@upnest\.com$/).test(email);
+      }
+    };
+
+  App.Validation = Validation;
+  window.App = App;
+}(window));
